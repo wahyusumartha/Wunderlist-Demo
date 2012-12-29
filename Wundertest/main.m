519cfde58dc45ed152adb919838b115196748c72
ogres <sergo_bero@hotmail.com>
2012-12-29 11:35:34 +0400

    Initial Commit

---
+++ Wundertest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wundertest
+//
+//  Created by Sergo Beruashvili on 12/29/12.
+//  Copyright (c) 2012 Sergo Beruashvili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
